51d8ddb95dd70e1a22cb1178c8b3b0c0453f7a27
Valentin Schneider <valentin.schneider@arm.com>
2019-12-11 11:38:50 +0000

    BACKPORT: sched/fair: Make task_fits_capacity() consider uclamp restrictions
    task_fits_capacity() drives CPU selection at wakeup time, and is also used
    to detect misfit tasks. Right now it does so by comparing task_util_est()
    with a CPU's capacity, but doesn't take into account uclamp restrictions.
    
    There's a few interesting uses that can come out of doing this. For
    instance, a low uclamp.max value could prevent certain tasks from being
    flagged as misfit tasks, so they could merrily remain on low-capacity CPUs.
    Similarly, a high uclamp.min value would steer tasks towards high capacity
    CPUs at wakeup (and, should that fail, later steered via misfit balancing),
    so such "boosted" tasks would favor CPUs of higher capacity.
    
    Introduce uclamp_task_util() and make task_fits_capacity() use it.
    
    [QP: fixed missing dependency on fits_capacity() by using the open coded
    alternative]
    
    Bug: 120440300
    Tested-By: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Quentin Perret <qperret@google.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20191211113851.24241-5-valentin.schneider@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit a7008c07a568278ed2763436404752a98004c7ff)
    Signed-off-by: Quentin Perret <qperret@google.com>
    Change-Id: Iabde2eda7252c3bcc273e61260a7a12a7de991b1
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3759,6 +3759,20 @@ unsigned long task_util_est(struct task_struct *p)
 	return max(task_util(p), _task_util_est(p));
 }
 
+#ifdef CONFIG_UCLAMP_TASK
+static inline unsigned long uclamp_task_util(struct task_struct *p)
+{
+	return clamp(task_util_est(p),
+		     uclamp_eff_value(p, UCLAMP_MIN),
+		     uclamp_eff_value(p, UCLAMP_MAX));
+}
+#else
+static inline unsigned long uclamp_task_util(struct task_struct *p)
+{
+	return task_util_est(p);
+}
+#endif
+
 static inline void util_est_enqueue(struct cfs_rq *cfs_rq,
 				    struct task_struct *p)
 {
@@ -3893,7 +3907,7 @@ static inline bool task_fits_capacity(struct task_struct *p,
 	else
 		margin = sched_capacity_margin_up[task_cpu(p)];
 
-	return capacity * 1024 > task_util_est(p) * margin;
+	return capacity * 1024 > uclamp_task_util(p) * margin;
 }
 
 static inline bool task_fits_max(struct task_struct *p, int cpu)
