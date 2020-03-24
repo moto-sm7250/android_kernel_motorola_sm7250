2cd5bfc87df05cfce16a4ec9d2caf46bbe7aa8c4
liaohj <liaohj@lenovo.com>
2020-03-24 19:24:45 +0800

    audio-kernel:update aw882xx v0.1.8 driver
    1.update aw882xx v0.1.8 driver
    2.add related afe & adm aw882xx function.
    
    Change-Id: I7ec18ab90842294dbb89d17932700b7c08ea82fb
    Signed-off-by: liaohj <liaohj@mt.com>
    Reviewed-on: https://gerrit.mot.com/1520433
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- asoc/codecs/aw882xx/aw882xx.h
+++ asoc/codecs/aw882xx/aw882xx.h
@@ -1,8 +1,7 @@
 #ifndef _AW882XX_H_
 #define _AW882XX_H_
 
-/*#define AW_DEBUG*/
-
+//#define AW_DEBUG
 /*
  * i2c transaction on Linux limited to 64k
  * (See Linux kernel documentation: Documentation/i2c/writing-clients)
@@ -17,8 +16,74 @@
 
 #define AW882XX_NUM_RATES					9
 #define AW882XX_SYSST_CHECK_MAX				10
+
 #define AW882XX_MODE_SHIFT_MAX				2
 
+#define AFE_RX_PROT_ID  0x1002				/*AFE_PORT_ID_SECONDARY_MI2S_RX */
+#define AW_MODULE_ID_COPP (0X10013D02)			/*SKT module id*/
+#define AW_MODULE_PARAMS_ID_COPP_ENABLE (0X10013D14)	/*SKT enable param id*/
+
+
+
+#define DEFAULT_CALI_VALUE (7)
+#define AFE_PARAM_ID_AWDSP_RX_SET_ENABLE        (0x10013D11)
+#define AFE_PARAM_ID_AWDSP_RX_PARAMS            (0x10013D12)
+#define AFE_PARAM_ID_AWDSP_TX_SET_ENABLE        (0x10013D13)
+#define AFE_PARAM_ID_AWDSP_RX_VMAX_L            (0X10013D17)
+#define AFE_PARAM_ID_AWDSP_RX_VMAX_R            (0X10013D18)
+#define AFE_PARAM_ID_AWDSP_RX_CALI_CFG_L        (0X10013D19)
+#define AFE_PARAM_ID_AWDSP_RX_CALI_CFG_R        (0x10013d1A)
+#define AFE_PARAM_ID_AWDSP_RX_RE_L              (0x10013d1B)
+#define AFE_PARAM_ID_AWDSP_RX_RE_R              (0X10013D1C)
+#define AFE_PARAM_ID_AWDSP_RX_NOISE_L           (0X10013D1D)
+#define AFE_PARAM_ID_AWDSP_RX_NOISE_R           (0X10013D1E)
+#define AFE_PARAM_ID_AWDSP_RX_F0_L              (0X10013D1F)
+#define AFE_PARAM_ID_AWDSP_RX_F0_R              (0X10013D20)
+#define AFE_PARAM_ID_AWDSP_RX_REAL_DATA_L       (0X10013D21)
+#define AFE_PARAM_ID_AWDSP_RX_REAL_DATA_R       (0X10013D22)
+
+
+enum AWINIC_CALI_CMD{
+	AW_CALI_CMD_NONE = 0,
+	AW_CALI_CMD_START,
+	AW_CALI_CMD_END,
+	AW_CALI_CMD_SET_CALI_CFG,
+	AW_CALI_CMD_GET_CALI_CFG,
+	AW_CALI_CMD_GET_CALI_DATA,
+	AW_CALI_CMD_SET_NOISE,
+	AW_CALI_CMD_GET_F0,
+	AW_CALI_CMD_SET_CALI_RE,
+	AW_CALI_CMD_GET_CALI_RE,
+	AW_CALI_CMD_SET_VMAX,
+	AW_CALI_CMD_GET_VMAX,
+	AW_CALI_CMD_MAX,
+};
+
+#define AW882XX_CALI_CFG_NUM 3
+#define AW882XX_CALI_DATA_NUM 6
+#define AW882XX_PARAMS_NUM 400
+struct cali_cfg{
+	int32_t data[AW882XX_CALI_CFG_NUM];
+};
+struct cali_data{
+	int32_t data[AW882XX_CALI_DATA_NUM];
+};
+struct params_data{
+	int32_t data[AW882XX_PARAMS_NUM];
+};
+
+#define AW882XX_IOCTL_MAGIC                'a'
+#define AW882XX_IOCTL_SET_CALI_CFG         _IOWR(AW882XX_IOCTL_MAGIC, 1, struct cali_cfg)
+#define AW882XX_IOCTL_GET_CALI_CFG         _IOWR(AW882XX_IOCTL_MAGIC, 2, struct cali_cfg)
+#define AW882XX_IOCTL_GET_CALI_DATA        _IOWR(AW882XX_IOCTL_MAGIC, 3, struct cali_data)
+#define AW882XX_IOCTL_SET_NOISE            _IOWR(AW882XX_IOCTL_MAGIC, 4, int32_t)
+#define AW882XX_IOCTL_GET_F0               _IOWR(AW882XX_IOCTL_MAGIC, 5, int32_t)
+#define AW882XX_IOCTL_SET_CALI_RE          _IOWR(AW882XX_IOCTL_MAGIC, 6, int32_t)
+#define AW882XX_IOCTL_GET_CALI_RE          _IOWR(AW882XX_IOCTL_MAGIC, 7, int32_t)
+#define AW882XX_IOCTL_SET_VMAX             _IOWR(AW882XX_IOCTL_MAGIC, 8, int32_t)
+#define AW882XX_IOCTL_GET_VMAX             _IOWR(AW882XX_IOCTL_MAGIC, 9, int32_t)
+#define AW882XX_IOCTL_SET_PARAM            _IOWR(AW882XX_IOCTL_MAGIC, 10,struct params_data)
+#define AW882XX_IOCTL_ENABLE_CALI          _IOWR(AW882XX_IOCTL_MAGIC, 11,int8_t)
 
 enum aw882xx_init {
 	AW882XX_INIT_ST = 0,
@@ -39,7 +104,7 @@ enum aw882xx_mode_spk_rcv {
 	AW882XX_SPEAKER_MODE = 0,
 	AW882XX_RECEIVER_MODE = 1,
 };
-/*smartpa monitor*/
+//smartpa monitor
 enum aw882xx_ipeak {
 	IPEAK_3P50_A = 0x08,
 	IPEAK_3P00_A = 0x06,
@@ -73,7 +138,7 @@ enum aw882xx_vmax_percentage {
 #define AW882XX_MONITOR_INT_10BIT 1023
 #define AW882XX_MONITOR_TEMP_SIGN_MASK (1<<9)
 #define AW882XX_MONITOR_TEMP_NEG_MASK (0XFC00)
-#define AW882XX_BIT_SYSCTRL2_BST_IPEAK_MASK ( 15<< 0)
+#define AW882XX_BIT_SYSCTRL2_BST_IPEAK_MASK         ( 15<< 0)
 #define AW882XX_BIT_HAGCCFG4_GAIN_SHIFT (8)
 #define AW882XX_BIT_HAGCCFG4_GAIN_MASK (0x00ff)
 
@@ -103,6 +168,36 @@ struct aw882xx_monitor{
 #endif
 };
 
+enum AWINIC_PROFILE{
+	AW_PROFILE_MUSIC = 0,
+	AW_PROFILE_RINGTONE,
+	AW_PROFILE_NOTIFICATION,
+	AW_PROFILE_VOICE,
+	AW_PROFILE_MAX,
+};
+
+#define VERSION_MAX 4
+#define PROJECT_NAME_MAX 24
+
+typedef struct awinic_afe_param_header{
+	uint8_t fw[VERSION_MAX];
+	uint8_t cfg[VERSION_MAX];
+	uint8_t project[PROJECT_NAME_MAX];
+	uint32_t start;
+	uint32_t params_len;
+	uint8_t check_sum;
+	uint8_t profile_num;
+	uint8_t reserve[2];
+}aw_afe_params_hdr_t;
+
+struct  profile_info {
+	struct mutex lock;
+	int cur_profile;
+	int status;
+	int len;
+	char* data[AW_PROFILE_MAX];
+};
+
 struct aw882xx {
 	struct regmap *regmap;
 	struct i2c_client *i2c;
@@ -126,15 +221,12 @@ struct aw882xx {
 	unsigned int init;
 	unsigned int spk_rcv_mode;
 	int32_t cali_re;
-	int32_t cali_f0;
 	unsigned int cfg_num;
+	struct  profile_info profile;
 };
 
 struct aw882xx_container {
 	int len;
 	unsigned char data[];
 };
-
-int aw882xx_set_cali_re(struct aw882xx *aw882xx, int32_t cali_re);
-
 #endif
