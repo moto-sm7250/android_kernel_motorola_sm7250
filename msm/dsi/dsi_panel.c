a30e1d2dfd6757fa8ccf6043a2032b2627b4f6f8
wengjun1 <wengjun1@motorola.com>
2020-05-22 15:58:13 +0800

    dsi_panel: send "panel_prepare" notify to touch
    Add a notify type "PANEL_EVENT_PANEL_PREPARE".
    After power on panel and reset completed, send this notify to
    Touch,that touch can start resume's work.
    
    Change-Id: I36d33d4c3ddc2222cfa4bbbfa4048e9fb83f4de0
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1572229
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Jun Weng <wengjun1@motorola.com>
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -4654,6 +4654,19 @@ exit:
 	return rc;
 }
 
+#if defined(CONFIG_PANEL_NOTIFICATIONS)
+static inline int dsi_panel_get_index(struct dsi_panel *panel)
+{
+	struct dsi_display *dsi_display =
+			container_of(panel->host, struct dsi_display, host);
+
+	if (unlikely(dsi_display == NULL))
+		return -EINVAL;
+
+	return dsi_display->display_idx;
+}
+#endif
+
 int dsi_panel_prepare(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -4687,6 +4700,7 @@ int dsi_panel_prepare(struct dsi_panel *panel)
 		       panel->name, rc);
 		goto error_disable_gpio;
 	}
+	PANEL_NOTIFY(PANEL_EVENT_PANEL_PREPARE);
 
 	goto end;
 error_disable_gpio:
@@ -4992,19 +5006,6 @@ int dsi_panel_post_switch(struct dsi_panel *panel)
 	mutex_unlock(&panel->panel_lock);
 	return rc;
 }
-
-#if defined(CONFIG_PANEL_NOTIFICATIONS)
-static inline int dsi_panel_get_index(struct dsi_panel *panel)
-{
-	struct dsi_display *dsi_display =
-			container_of(panel->host, struct dsi_display, host);
-
-	if (unlikely(dsi_display == NULL))
-		return -EINVAL;
-
-	return dsi_display->display_idx;
-}
-#endif
 
 int dsi_panel_enable(struct dsi_panel *panel)
 {
