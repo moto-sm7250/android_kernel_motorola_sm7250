b181b59f26ce95df6f10d4d1dd32f70aac1c5428
houdz1 <houdz1@lenovo.com>
2020-05-26 16:14:15 +0800

    dsi_panel: fix out-of-bounds  issue
    Before make the read operation, malloc the space of rx_buf.
    To avoid the out-of-bounds  issue.
    
    Change-Id: I6841d230af04b9a57b7281d8edc2e367e7f0ceab
    Signed-off-by: houdz1 <houdz1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1579756
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiaojun Ji <jixj@motorola.com>
    Reviewed-by: Shanshan Dai <daiss1@motorola.com>
    Reviewed-by: Hua Tan <tanhua1@motorola.com>
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Hujun Liao <liaohj@motorola.com>
    Submit-Approved: Jira Key

--- msm/dsi/dsi_panel.c
+++ msm/dsi/dsi_panel.c
@@ -3800,6 +3800,7 @@ static int dsi_panel_get_pwr_mode(struct dsi_panel *panel, u8 *val)
 	struct dsi_display *display = NULL;
 	u32 flags;
 	u8 payload = MIPI_DCS_GET_POWER_MODE;
+	u32 rx_buf;
 
 	display = container_of(panel->host, struct dsi_display, host);
 	if (!display) {
@@ -3819,7 +3820,7 @@ static int dsi_panel_get_pwr_mode(struct dsi_panel *panel, u8 *val)
 	cmd.msg.tx_len = 1;
 	cmd.msg.tx_buf = &payload;
 	cmd.msg.rx_len = 1;
-	cmd.msg.rx_buf = val;
+	cmd.msg.rx_buf = &rx_buf;
 
 	rc = dsi_display_cmd_mipi_transfer(display, &cmd.msg, flags);
 	if (rc <= 0) {
@@ -3827,6 +3828,8 @@ static int dsi_panel_get_pwr_mode(struct dsi_panel *panel, u8 *val)
 		rc = -EIO;
 	} else
 		rc = 0;
+
+	*val = rx_buf & 0xFF;
 end:
 	return rc;
 
