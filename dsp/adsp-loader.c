5da865ce412efd3dbaa6ff3435ab677b2090a8e2
huangys <huangys@motorola.com>
2020-07-15 14:58:43 +0800

    dsp: adsp-loader: restart reqeust panic for ramdump
    adsp restart request is not normal. It's an action of "last resort"
    for function recovery. We hope panic happen for ramdump generation
    in userdebug build.
    
    Change-Id: I19f5ff395f6b55faa41db47b599432ec146c03a4
    Signed-off-by: huangys <huangys@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1683745
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- dsp/adsp-loader.c
+++ dsp/adsp-loader.c
@@ -193,6 +193,9 @@ static ssize_t adsp_ssr_store(struct kobject *kobj,
 
 	dev_err(&pdev->dev, "requesting for ADSP restart\n");
 
+#if defined(ADSP_RESTART_DEBUG)
+	panic("%s: panic for getting ramdump in userdebug release", __func__);
+#endif
 	/* subsystem_restart_dev has worker queue to handle */
 	rc = subsystem_restart_dev(adsp_dev);
 	if (rc) {
