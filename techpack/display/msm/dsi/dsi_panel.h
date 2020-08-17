8c1c35cd8da0cc2d290ef0098f271f7fb3f6a398
Demon000 <demonsingur@gmail.com>
2020-08-18 01:11:56 +0200

    drm: msm: implement fod handling
    Change-Id: Ib4eac52663d8c54f9b09892c537ef5e4f0cddf73

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -246,6 +246,12 @@ enum touch_state {
 	TOUCH_LOW_POWER_STATE,
 };
 
+#define BRIGHTNESS_ALPHA_PAIR_LEN 2
+struct brightness_alpha_pair {
+	u32 brightness;
+	u32 alpha;
+};
+
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -324,6 +330,9 @@ struct dsi_panel {
 
 	int panel_recovery_retry;
 	bool is_panel_dead;
+
+	struct brightness_alpha_pair *fod_dim_lut;
+	u32 fod_dim_lut_count;
 };
 
 bool dsi_display_all_displays_dead(void);
@@ -460,5 +469,9 @@ int dsi_panel_get_elvss_data(struct dsi_panel *panel);
 int dsi_panel_get_elvss_data_1(struct dsi_panel *panel);
 int dsi_panel_set_elvss_dim_off(struct dsi_panel *panel, u8 val);
 int dsi_panel_parse_elvss_config(struct dsi_panel *panel, u8 elv_vl);
+
+int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
+
+u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
