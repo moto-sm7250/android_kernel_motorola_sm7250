257fbbea22cffdffee8aed42312b9a722d495303
Demon000 <demonsingur@gmail.com>
2020-08-18 01:14:55 +0200

    drm: msm: notify sysfs for fod ui changes

--- techpack/display/msm/dsi/dsi_display.h
+++ techpack/display/msm/dsi/dsi_display.h
@@ -286,6 +286,7 @@ struct dsi_display {
 	u32 clk_gating_config;
 	bool queue_cmd_waits;
 	struct workqueue_struct *dma_cmd_workq;
+	atomic_t fod_ui;
 };
 
 int dsi_display_dev_probe(struct platform_device *pdev);
@@ -778,5 +779,7 @@ int dsi_display_get_panel_vfp(void *display,
 	int h_active, int v_active);
 
 struct dsi_display *get_main_display(void);
+
+void dsi_display_set_fod_ui(struct dsi_display *display, bool status);
 
 #endif /* _DSI_DISPLAY_H_ */
