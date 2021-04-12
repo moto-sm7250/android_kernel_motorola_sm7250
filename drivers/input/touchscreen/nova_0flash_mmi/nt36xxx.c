9fdcd46ebdfe312fa5a9cbd6ee675132d61a483a
electimon <electimon@gmail.com>
2021-04-12 20:40:50 +0800

    drivers: input: touchscreen: nova_0flash_mmi: Load firmware earlier
    * stupid code
    
    Signed-off-by: Jhon Diaz <electimon@gmail.com>
    Change-Id: I4bd5108efc438ad2cb4323c356aacfd8b2ba3ea4

--- drivers/input/touchscreen/nova_0flash_mmi/nt36xxx.c
+++ drivers/input/touchscreen/nova_0flash_mmi/nt36xxx.c
@@ -2481,7 +2481,7 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	}
 	INIT_DELAYED_WORK(&ts->nvt_fwu_work, Boot_Update_Firmware);
 	// please make sure boot update start after display reset(RESX) sequence
-	queue_delayed_work(nvt_fwu_wq, &ts->nvt_fwu_work, msecs_to_jiffies(14000));
+	queue_delayed_work(nvt_fwu_wq, &ts->nvt_fwu_work, msecs_to_jiffies(7000));
 #endif
 #ifdef LCM_FAST_LIGHTUP
 	INIT_WORK(&ts_resume_work, nova_resume_work_func);
